2b0e97836d196af76946362315d699d44bf21997
kevin <kevin@10.10.20.15>
2014-08-25 09:20:01 +0800

    wordcountTest

---
+++ src/org/apache/hadoop/remoteconnection/WordCount.java
@@ -0,0 +1,77 @@
+package org.apache.hadoop.remoteconnection;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+
+public class WordCount {
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+				// System.out.println(word+":"+one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				System.out.println(key + ":" + val.get());
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new YarnConfiguration();
+		conf.set("fs.defaultFS", "hdfs://master:8020");
+		conf.set("mapreduce.framework.name", "yarn");
+		conf.set("yarn.resourcemanager.address", "master:8032");
+
+		Path input = new Path("hdfs://master:8020/word.txt");
+		Path output = new Path("hdfs://master:8020/out");
+		FileSystem fs = FileSystem.get(conf);
+		boolean a = fs.delete(output, true);
+		System.out.println(a);
+		Job job = new Job(conf);
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, input);
+		FileOutputFormat.setOutputPath(job, output);
+		job.waitForCompletion(true);
+	}
+}
